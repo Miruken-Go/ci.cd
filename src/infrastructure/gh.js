654c1d0f1169c2756ae993dfe6299066821c07ca
Michael  Dudley <provenstyle@gmail.com>
2023-12-01 13:52:51 -0600

    adding sendRepositoryDispatch and using env variables

--- src/infrastructure/gh.js
+++ src/infrastructure/gh.js
@@ -1,5 +1,6 @@
 import * as bash   from './bash.js'
 import { secrets } from './envSecrets.js'
+import { variables } from './envVariables.js'
 import axios       from 'axios'
 
 secrets.require([
@@ -7,23 +8,43 @@ secrets.require([
     'GH_TOKEN'
 ])
 
-export async function sendRepositoryDispatches(githubOrg, eventType, payload) { 
+variables.requireEnvVariables([
+    'repository',
+    'repositoryOwner'
+])
+
+variables.optionalEnvVariables([
+    'skipRepositoryDispatches'
+])
+
+export async function sendRepositoryDispatch(eventType, payload, repository) { 
+    if (variables.skipRepositoryDispatches.to) return
+
+    const repo = repository || variables.repository
+    if (!repo) throw new Error("Repository name is required")
+
+    await axios.post(`https://api.github.com/repos/${variables.repositoryOwner}/${repo}/dispatches`, {
+        event_type:     eventType,
+        client_payload: payload
+    }, {
+        headers: {
+            Accept: 'application/vnd.github+json',
+            Authorization: `Bearer ${secrets.ghToken}`,
+            "X-GitHub-Api-Version": '2022-11-28'
+        }
+    })
+
+    console.log(`Sent [${eventType}] repository dispatch to [${variables.repository}] with data [${JSON.stringify(payload)}]`)
+}
+
+export async function sendRepositoryDispatches(eventType, payload) { 
+    if (variables.skipRepositoryDispatches) return
+
     const repos = await bash.json(`
-        gh repo list ${githubOrg} --json name
+        gh repo list ${variables.repositoryOwner} --json name
     `)
 
     for (const repo of repos) {
-        await axios.post(`https://api.github.com/repos/${githubOrg}/${repo.name}/dispatches`, {
-            event_type:     eventType,
-            client_payload: payload
-        }, {
-            headers: {
-                Accept: 'application/vnd.github+json',
-                Authorization: `Bearer ${secrets.ghToken}`,
-                "X-GitHub-Api-Version": '2022-11-28'
-            }
-        })
-
-        console.log(`Sent [${eventType}] repository dispatch to [${repo.name}] with data [${JSON.stringify(payload)}]`)
+        await sendRepositoryDispatch(eventType, payload, repo.name)
     }
 }
