a6edc75cd4e035f0a2f1d9a97d30b4c5629f3319
Michael  Dudley <provenstyle@gmail.com>
2023-12-28 00:09:15 -0600

    adding containerApp function

---
+++ src/infrastructure/containerApp.js
@@ -0,0 +1,23 @@
+import * as bash from '#infrastructure/bash.js'
+
+export async function getImageTagForActiveRevision(application) { 
+    const revisions = await bash.json(`
+        az containerapp revision list                 \
+            -n ${application.containerAppName}        \
+            -g ${application.resourceGroups.instance} \
+    `)
+    const active = revisions.filter(r => 
+        r.properties.active == true &&
+        r.properties.trafficWeight == 100
+    )
+
+    if (active.length) {
+        const image = active[0].properties.template.containers[0].image
+        const split = image.split(':')
+        return split.length > 1
+            ? split[1]
+            : null
+    } else {
+        return null
+    }
+}
