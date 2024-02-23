955ef514e8f4f16ef40c62522d3dc070f236d4b6
Michael  Dudley <provenstyle@gmail.com>
2024-02-22 22:32:28 -0600

    container app may not exist especially the first time

--- src/infrastructure/containerApp.js
+++ src/infrastructure/containerApp.js
@@ -1,23 +1,29 @@
 import * as bash from '#infrastructure/bash.js'
 
 export async function getImageTagForActiveRevision(application) { 
-    const revisions = await bash.json(`
-        az containerapp revision list                 \
-            -n ${application.containerAppName}        \
-            -g ${application.resourceGroups.instance} \
-    `)
-    const active = revisions.filter(r => 
-        r.properties.active == true &&
-        r.properties.trafficWeight == 100
-    )
+    try {
+        const revisions = await bash.json(`
+            az containerapp revision list                 \
+                -n ${application.containerAppName}        \
+                -g ${application.resourceGroups.instance} \
+        `)
+        const active = revisions.filter(r => 
+            r.properties.active == true &&
+            r.properties.trafficWeight == 100
+        )
 
-    if (active.length) {
-        const image = active[0].properties.template.containers[0].image
-        const split = image.split(':')
-        return split.length > 1
-            ? split[1]
-            : null
-    } else {
-        return null
+        if (active.length) {
+            const image = active[0].properties.template.containers[0].image
+            const split = image.split(':')
+            return split.length > 1
+                ? split[1]
+                : null
+        } else {
+            return null
+        }
+    } catch {
+        console.log(`no active container app revision for ${application.containerAppName}`)
     }
+
+    return null
 }
