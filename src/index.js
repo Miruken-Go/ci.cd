910720c04800954863ad14870fbecb905a794ae6
Michael  Dudley <provenstyle@gmail.com>
2024-03-08 10:48:07 -0600

    exposing all modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
-import * as handler from '#infrastructure/handler'
-import * as bash from '#infrastructure/bash'
+import * as az           from '#infrastructure/az.js'
+import * as b2c          from '#infrastructure/b2c.js'
+import * as bash         from '#infrastructure/bash.js'
+import * as config       from '#infrastructure/config.js'
+import * as containerApp from '#infrastructure/containerApp.js'
+import * as envSecrets   from '#infrastructure/envSecrets.js'
+import * as envVariables from '#infrastructure/envVariables.js'
+import * as gh           from '#infrastructure/gh.js'
+import * as git          from '#infrastructure/git.js'
+import * as go           from '#infrastructure/go.js'
+import * as graph        from '#infrastructure/graph.js'
+import * as handler      from '#infrastructure/handler.js'
+import * as keyVault     from '#infrastructure/keyvaultz.js'
+import * as logging      from '#infrastructure/logging.js'
+import * as users        from '#infrastructure/users.js'
 
 export {
+    az,
+    b2c,
+    bash,
+    config,
+    containerApp,
+    envSecrets,
+    envVariables,
+    gh,
+    git,
+    go,
+    graph,
     handler,
-    bash
+    keyVault,
+    logging,
+    users
 }
