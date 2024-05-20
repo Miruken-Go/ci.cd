32f4b7fafa4bb650e4d3ee1c4f8a4b0c6b4460c6
michael dudley <michael.dudley@improving.com>
2024-05-20 12:31:02 -0500

    adding type

--- src/infrastructure/versionNumbers.ts
+++ src/infrastructure/versionNumbers.ts
@@ -19,7 +19,7 @@ export async function repoVersion (repositoryPath: string, ref: string): Promise
 //This docker container is running docker in docker from github actions
 //Therefore using $(pwd) to get the working directory would be the working directory of the running container 
 //Not the working directory from the host system. So we need to pass in the repository path.
-export async function tagVersion (repositoryPath: string, ref: string, tagPrefix): Promise<string> {
+export async function tagVersion (repositoryPath: string, ref: string, tagPrefix: string): Promise<string> {
     const versionFormat = (ref === 'refs/heads/main') ? 'MajorMinorPatch' : 'SemVer'
     const rawVersion = await execute(`
         docker run --rm -v '${repositoryPath}:/repo' \
